d2db763c07a1cce1d847e7877f6d54c164d98f34
proll4588 <prol4588@gmail.com>
2022-12-25 23:11:20 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import "./styles/style.scss";
+
+// import "./js/jquery-3.6.0.min.js";
+import "./js/slick.min.js";
+import $ from "jquery";
+
+// $(document).ready(function () {
+//     $(".slider").slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         infinite: false,
+//         variableWidth: true,
+//         outerEdgeLimit: true,
+//     });
+// });
+
+$(document).ready(function () {
+    $(".slider").slick({
+        centerMode: true,
+        // centerPadding: "100px",
+        // slidesToShow: 1,
+        variableWidth: true,
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: "40px",
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: "40px",
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    });
+});
